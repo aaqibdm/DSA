012972b8d5a6ab19547688fdea0a909f9214bc5f
Aaqib DM <aaqibdm7@gmail.com>
2024-08-16 18:06:03 +0530

    Create sum.cpp

---
+++ BASIC/sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b,c;
+    Cout<<"Enter the First number : ";
+    cin>>a;
+    cout<<"Enter the Second number : ";
+    cin>>b;
+    c=a+b;
+    cout<<c;
+    return 0();
+}
