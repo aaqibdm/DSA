5dd538e46c5f20d7e2eac3f1ae380d9e02d58837
Aaqib DM <aaqibdm7@gmail.com>
2024-08-17 20:51:03 +0530

    Update swap.cpp

--- BASIC/swap.cpp
+++ BASIC/swap.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int a,b,c;
+    
+    int a,b,c; //swap number using third variable
+    
     cout<<" Enter the first number : ";
+    
     cin>>a;
+    
     cout<<"Enter the second number : ";
+    
     cin>>b;
+    
     c=a;
+    
     a=b;
+    
     b=c;
+    
     cout<<"a="<<a<<endl;
+
     cout<<"b="<<b<<endl;
 }
