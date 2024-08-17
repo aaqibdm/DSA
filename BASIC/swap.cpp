8f03d87a0305068f3381b89ecab52cdc1b8f3e4e
Aaqib DM <aaqibdm7@gmail.com>
2024-08-17 20:48:12 +0530

    Create swap.cpp

---
+++ BASIC/swap.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<" Enter the first number : ";
+    cin>>a;
+    cout<<"Enter the second number : ";
+    cin>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b<<endl;
+}
