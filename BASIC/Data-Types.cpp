e4e08dd187cf184277f29654f3ad6f5d59a08154
Aaqib DM <aaqibdm7@gmail.com>
2024-08-21 17:42:08 +0530

    Create Data-Types.cpp

---
+++ BASIC/Data-Types.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    // int a = 5;               // Integer (Stores whole numbers, without decimals) 
+    // float b = 5.99;     // Floating point number (Sufficient for storing 6-7 decimal digits)
+    // double c = 9.98;   // Floating point number (Sufficient for storing 15 decimal digits)
+    // char d = 'D';         // Character (Stores a single character,latter)
+    // bool e = true;       // Boolean  (Stores true or false values)
+    // string f = "Hello";     // String  (Stores a lines of characte)
+    int a ;
+    cout<<"Enter integer: ";
+    cin>>a;
+     
+    float b ;
+    
+    cout<<"Enter float: ";
+    cin>>b;
+
+    char c ;
+    cout<<"Enter char: ";
+    cin>>c;
+
+    cout<<"int="<<a;
+    cout<<"float="<<b;
+    cout<<"char="<<c;
+
+
+}
