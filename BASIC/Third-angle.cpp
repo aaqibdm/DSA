a9175b93c9d9a3a2f47c81af6621bc015e029a4a
Aaqib DM <aaqibdm7@gmail.com>
2024-08-22 19:30:06 +0530

    Create Third-angle.cpp

---
+++ BASIC/Third-angle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int angle1,angle2,angle3;
+
+    cout<<"Enter the angle 1 : ";
+
+    cin>>angle1;
+
+    cout<<"Enter the angle 2 : ";
+
+    cin>>angle2;
+
+    angle3=180-(angle1+angle2);
+
+    cout<<"Angle 3 = "<<angle3;
+
+}
